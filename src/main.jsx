86faa0b85f62c567a3609af2728a61ec9ce55709
fjpachecousa-wq <fj.pacheco.usa@gmail.com>
2025-09-10 20:04:33 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// Se tiver um CSS global, descomente a linha abaixo e crie src/index.css
+// import "./index.css";
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
