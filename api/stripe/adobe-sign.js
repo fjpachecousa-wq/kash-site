7c32e55572d7dcd2dcd863a2825ab6086b7b667d
fjpachecousa-wq <fj.pacheco.usa@gmail.com>
2025-09-16 22:35:18 -0400

    Delete api/stripe/adobe-sign.js

--- api/stripe/adobe-sign.js
+++
@@ -1,9 +0,0 @@
-// /api/webhooks/adobe-sign.js
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
-
-  // TODO: validar assinatura do Adobe Sign e atualizar status (agreement signed)
-  console.log("Adobe Sign webhook received (stub). Body:", req.body);
-  return res.status(200).json({ ok: true });
-}
-
